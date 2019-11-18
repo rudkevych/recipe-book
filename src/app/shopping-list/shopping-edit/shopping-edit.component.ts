609a797c942dea87e6194361ed77424cf9d4fa0d
Oksana Rudkevych <oksana.rudkevych@gmail.com>
2019-11-18 10:30:23 +0200

    shopping-edit form refactoring using template-driven approach

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
 import { ShoppingListService } from './../shopping-list.service';
 import { Ingredient } from './../../shared/ingredient.model';
 import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,18 +9,15 @@ import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('newIngredientName', {static: true}) newIngredientName: ElementRef;
-  @ViewChild('newIngredientAmount', {static: true}) newIngredientAmount: ElementRef;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onAddItem() {
-    const ingredientName = this.newIngredientName.nativeElement.value;
-    const ingredientAmount = this.newIngredientAmount.nativeElement.value;
-    const newIngredient = new Ingredient(ingredientName, ingredientAmount);
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.shoppingListService.addNewIngredient(newIngredient);
   }
 
