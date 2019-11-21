19e57317a200f7de8a50fd5644dd18c57d41d9b5
Oksana Rudkevych <oksana.rudkevych@gmail.com>
2019-11-21 14:10:23 +0200

    better error handling

--- src/app/auth/auth/auth.service.ts
+++ src/app/auth/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { error } from 'util';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs'
 import { catchError } from 'rxjs/operators';
@@ -28,27 +28,39 @@ export class AuthService {
           password,
           returnSecureToken: true
         }
-      ).pipe(catchError(errorRes => {
-        let errorMessage = 'An unknown error occurred';
-        if(!errorRes.error || !errorRes.error.error) {
-          return throwError(errorMessage);
-        }
-        switch (errorRes.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = 'This email is already used!';
-        }
-        return throwError(errorMessage);
-      }));
+      ).pipe(catchError(this.handleError));
   }
 
   logIn(email: string, password: string) {
     return this.http.post<AuthResponseData>
-    ('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDfIiXzxI72D5lNlbGGpZszVu_ak0ZhKNk',
-    {
-      email,
-      password,
-      returnSecureToken: true
-    });
+      ('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDfIiXzxI72D5lNlbGGpZszVu_ak0ZhKNk',
+        {
+          email,
+          password,
+          returnSecureToken: true
+        }).pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email is already used!';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'Cant`t find such email. Try to register!';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'Password is not correct. Please, try again';
+        break;
+      case 'USER_DISABLED':
+        errorMessage = 'User is disabled. write us admin@gmail.com';
+        break;
+    }
+    return throwError(errorMessage);
   }
 
 }
