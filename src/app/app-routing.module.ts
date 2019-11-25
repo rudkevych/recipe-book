e9309be1f27e57b51bdd4f5ac88a18867f0896d5
Oksana Rudkevych <oksana.rudkevych@gmail.com>
2019-11-25 13:26:55 +0200

    refactor to new syntax recipe lazy-loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'}
+  { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule) }
 ];
 
 @NgModule({
